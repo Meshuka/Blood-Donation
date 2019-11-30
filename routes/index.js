9c93d36e0a33c522da06b540a64acbc489e41517
Meshuka <meshukarayamajhi@gmail.com>
2019-11-30 13:07:10 +0545

    ejs pages

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/home', function(req, res, next) {
   res.render('first', { title: 'Express' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+router.get('/blog', function(req, res, next) {
+  res.render('blog');
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
 });
 
 router.get('/events', function(req, res, next) {
